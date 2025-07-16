7ecbcd3c3d88e05451047f2e6be9ff3263cfd966
Belfos <belfosstuff@gmail.com>
2025-07-16 17:50:40 +0200

    Add files via upload

---
+++ d1ex1.py
@@ -0,0 +1,5 @@
+nombre=input("Nombre? ")
+frase="Hola "+nombre+" bienvenido e tu primer bot de python"
+x=1
+for x in range(3):
+    print(frase)
---
+++ d1ex2.py
@@ -0,0 +1,45 @@
+print("Hola Mundo de las automatizaciones con python")
+
+#Variables
+
+nombre = "ChatGPT"
+
+edad = 3
+
+if edad > 2:
+    print("Eres viejo para una IA")
+
+
+#Bucles
+
+for i in range(5):
+    print("contando", i)
+
+#len #para medir los caracteres
+
+# .index() #Mide la posicion en la que se encuentra()
+
+# .count() #cuenta la cantidad de caracteres ()
+
+# .split
+
+# .endswith
+
+# .startswith
+
+# .upper
+
+# .lower
+
+# and
+
+# or
+
+name = "Pablo"
+
+age = 24
+
+if name == "Pablo" and age == 24:
+    print("eres pablo y tienes 24")
+
+
